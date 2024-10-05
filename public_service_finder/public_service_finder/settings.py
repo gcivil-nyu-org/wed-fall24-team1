c918c3c951b92626cf20fc463a2bad6df8d148a1
Deepjyot Singh Kapoor <kapoordeepjyotsingh29@gmail.com>
2024-10-05 00:18:30 -0400

    changed secret key

--- public_service_finder/public_service_finder/settings.py
+++ public_service_finder/public_service_finder/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qkdoa^_d-cnf1)7u!pvlq1i6s9^5(__iaiczz94&e4hz1@4=26'
+# SECRET_KEY = 'django-insecure-qkdoa^_d-cnf1)7u!pvlq1i6s9^5(__iaiczz94&e4hz1@4=26'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
