7c0a0544ff92fdc53268c6b3d4c8d2ed9a197f59
Shubham Garg <sg8311@nyu.edu>
2024-11-26 02:09:32 -0500

    (#215) Password reset (#218)
    * Password reset chnages
    
    * linting
    
    * config changes
    
    * UI chnages

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -24,4 +24,34 @@ urlpatterns = [
         "logout/", auth_views.LogoutView.as_view(next_page="user_login"), name="logout"
     ),  # Logout URL
     path("", include("allauth.urls")),  # This allows allauth URLs under /accounts/
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="password_reset.html",
+            email_template_name="password_reset_email.html",
+            subject_template_name="password_reset_subject.txt",
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
--- src/public_service_finder/settings.py
+++ src/public_service_finder/settings.py
@@ -81,6 +81,21 @@ LOGIN_REDIRECT_URL = "home"  # Redirect to the home page after login
 LOGOUT_REDIRECT_URL = "user_login"  # Redirect to user login after logout
 
 
+# settings.py
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"  # Example for Gmail
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config("SMTP_USER")
+EMAIL_HOST_PASSWORD = config("SMTP_PASSWORD")
+DEFAULT_FROM_EMAIL = config("SMTP_USER")
+# settings.py
+
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# DEFAULT_FROM_EMAIL = "noreply@yourdomain.com"
+
+
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
